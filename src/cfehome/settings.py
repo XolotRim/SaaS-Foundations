5917df01bbd3bc051ce6834f7e0e70de06fb1ec4
Talha Mumtaz <talhaamumtaz@gmail.com>
2024-10-12 14:08:58 +0500

    updated code with playlist|Prepared for railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-180c#3_a#lkayl@qldfkbdi*o8pc0v2=-9#af=$)udo0@&%4er
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -3,14 +3,22 @@ from django.shortcuts import render
 
 from visits.models import PageVisit
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args , **kwargs)
+
+def about_view(request,*args,**kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "My Page"
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count(),
+    except:
+        percent = 0
     my_context = {
         "page_title": my_title,
         "page_visit_count" : page_qs.count,
-        "total_visit_count" : qs.count
+        "total_visit_count" : qs.count,
+        "percent" : percent
     }
     path = request.path
     html_template = "home.html"
